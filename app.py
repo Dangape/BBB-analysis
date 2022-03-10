a666cf519f430adcb70b49bfb85f0e632c256f4f
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2022-03-10 12:51:54 -0300

    aws working

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
+
+import json
+import os
 import tweepy
-import pandas as pd
-import nltk
 import re
 from unidecode import unidecode
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
-import spacy
-import logging
-from config import create_api
 from datetime import datetime
-import schedule
 import time
-from io import BytesIO
+import nltk
+import pandas as pd
 import io
+import pytz
 
-nlp = spacy.load('en_core_web_sm')
-nltk.download('vader_lexicon')
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
+consumer_key = 'AxrLnGCzWymdqtyaGyuPps5oa'  # API key
+consumer_secret = 'dDZP1s8kCO5fg2yM3sVv60cFb0Zhmj0DT2cgh5ZneJDUEerhQM' # API key secret
+access_token = '1460657321839890436-ZnRI0HMOYTVNpWh7j0QIM4m62G4qCo'  # Access token
+access_token_secret ='janLV9AZyllBqORJJfltkegaeYISDNTbflUZZtCLWmgEB'  # Access token secret
 
-api = create_api()
-keyword = '#BBB22 OR #bbb22'
-n_tweets = 1000
+# logger = logging.getLogger()
+nltk.data.path.append("/tmp")
+nltk.download('stopwords',download_dir = "/tmp")
 
 stopwords = nltk.corpus.stopwords.words('portuguese')
 newStopWords = ['né','Se','q','vc','ter','ne','da','to','tô','https','BBB22','tá',
@@ -32,14 +31,34 @@ newStopWords = ['né','Se','q','vc','ter','ne','da','to','tô','https','BBB22','
                 'co','t','n','desde','so','mim''la','quer','fez','agora','aqui','vcs','gente','deu', 'ate', 'oq', 'ser']
 
 stopwords.extend(newStopWords)
-tweet_list = []
 
-# datetime object containing current date and time
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+def handler(event, context):
+    # TODO implement
+    create_wc()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Tweet with Success using AWS Lambda!')
+    }
 
 def create_wc():
-    logger.info("Getting tweets")
+    # Authentication
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    # logger.info("API created")
+
+    keyword = '#BBB22 OR #bbb22'
+    n_tweets = 1000
+
+
+    tweet_list = []
+
+    # datetime object containing current date and time
+    tz = pytz.timezone('America/Sao_Paulo')
+    ct = datetime.now(tz=tz)
+    dt_string = ct.strftime("%d/%m/%Y %H:%M:%S")
+    # logger.info("Getting tweets")
     for tweet in tweepy.Cursor(api.search_tweets, q=keyword, lang = 'pt').items(n_tweets):
         tweet_list.append(unidecode(tweet.text))
     # #cleaning tweets
@@ -51,13 +70,13 @@ def create_wc():
     # Lowercase
     tw_list['text'] = tw_list.text.str.lower()
 
-    #Remove RT
+    # Remove RT
     remove_rt = lambda x: re.sub('rt @\w+: ', ' ', x)
 
-    #Remove tags
+    # Remove tags
     tags = lambda x: re.sub(' /<[^>]+>/', ' ', x)
 
-    #Remove links
+    # Remove links, mentions, hashtags, laughts
     links = lambda x: re.sub('/(?:https?|ftp):\/\/[\n\S]+/g', ' ', x)
     hashtags = lambda x: re.sub('/\#\w\w+\s?/g', ' ', x)
     mentions = lambda x: re.sub('/\@\w\w+\s?/g', ' ', x)
@@ -72,16 +91,11 @@ def create_wc():
     tw_list['text'] = tw_list.text.map(laughts_ha)
     tw_list['text'] = tw_list.text.map(laughts_k)
 
-    #Remove stopwords
+    # Remove stopwords
     tw_list['text'] = tw_list['text'].apply(lambda x: ' '.join([x.strip() for x in x.split() if x not in stopwords]))
 
-    # create excel writer object
-    writer = pd.ExcelWriter('output.xlsx')
-    tw_list.to_excel(writer, engine='xlsxwriter')
-    writer.save()
-
     # create a wordcloud
-    logger.info("Generating WC")
+    # logger.info("Generating WC")
     wc = WordCloud(background_color='white',
                    collocations=False,
                    width=1600,
@@ -89,36 +103,17 @@ def create_wc():
                    contour_width=3,
                    contour_color='black',
                    stopwords=stopwords).generate(str(tw_list['text'].values))
-    plt.figure( figsize=(20,10), facecolor='k')
+    plt.figure( figsize=(20 ,10), facecolor='k')
     plt.imshow(wc, interpolation="bilinear")
     plt.axis("off")
     plt.tight_layout(pad=0)
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
-    logger.info("Tweetting")
     response = api.media_upload(filename="wordcloud", file=buf)
 
-    status = 'BBB em: ' + dt_string
-    api.update_status(status = status,media_ids=[response.media_id_string])
+    status = 'BBB EM: ' + dt_string + '#BBB22 #bbb22'
+    api.update_status(status = status ,media_ids=[response.media_id_string])
 
-
-create_wc()
-# # The Scheduling is happening below:
-# schedule.every().day.at("10:30").do(create_wc)
-# schedule.every().day.at("21:30").do(create_wc)
-# schedule.every().day.at("22:30").do(create_wc)
-# schedule.every().day.at("23:00").do(create_wc)
-# schedule.every().day.at("23:30").do(create_wc)
-# schedule.every().day.at("00:30").do(create_wc)
-#
-#
-# while True:
-#     try:
-#         schedule.run_pending()
-#         time.sleep(1)
-#     except:
-#         print('Error...')
-#         time.sleep(120)
 
 
